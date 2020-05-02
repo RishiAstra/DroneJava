8369ad1dde51f3cf57d8a77f35e9fb8c2a76e239
Rishi Astra <40644327+bobthefroggywoggy@users.noreply.github.com>
2020-05-01 23:35:18 -0400

    s

--- led/src/Main.java
+++ led/src/Main.java
@@ -1,6 +1,7 @@
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
+import jdk.jshell.spi.ExecutionControl;
 
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -8,6 +9,9 @@ import java.util.Scanner;
 
 //TODO: sending "hi hi" and making the arduino return it results in "hihi", all spaces lost
 public class Main {
+    public static double x;
+    public static double y;
+    public static double z;
 
     public static ArrayList<String> commandsToSend;//the commands/data to be sent out. Each ends in ",". The command at index 0 is repeatedly sent and removes till the size() is 0
     public static ArrayList<String> commandsReceived;//the commands/data creceived. Each ends in ",". Nothing is done with these at present
@@ -21,31 +25,11 @@ public class Main {
     static String serialCommandInProgress = "";
 
 
-    //this reads data
-    private static final class DataListener implements SerialPortDataListener
-    {
-        @Override
-        public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
-
-        @Override
-        public void serialEvent(SerialPortEvent event)
-        {
-            try {
-                //System.out.println("In event listener.");
-                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
-                    return;
-                //System.out.println("Past event type check.");
-                byte[] newData = new byte[comPort.bytesAvailable()];
-                int numRead = comPort.readBytes(newData, newData.length);
-                stringBuffer = new String(newData, "ASCII");//new String(newData,0,numRead);
-                //System.out.println("Read " + numRead + " bytes.");
-                System.out.println("RECIEVED: " + stringBuffer);
-                serialInputUnprocessed.add(stringBuffer);
-
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
+    public static void SetTargetPosition(Double xpos, Double ypos, Double zpos){
+        commandsToSend.add("t:" + xpos.toString() + ":"+ ypos.toString() + ":"+ zpos.toString() + ",");
+    }
+    public static void DropCargo(){
+        commandsToSend.add("d,");
     }
 
     //initialization stuff
@@ -67,60 +51,14 @@ public class Main {
         (new Thread(new SerialWriter(comPort.getOutputStream()))).start();
         (new Thread(new ConsoleCommands())).start();
         (new Thread(new InputsToCommands())).start();
+        (new Thread(new ProcessCommands())).start();
+
     }
 
-    //this writes out the commandsToSend to the arduino and says "SENDING: "
-    public static class SerialWriter implements Runnable
-    {
-        OutputStream out;
-
-        public SerialWriter ( OutputStream out )
-        {
-            this.out = out;
-
-        }
 
 
-        public void run ()
-        {
-            try{
 
-                while(true){
-                    /////sends commandsToSend
-                    while(commandsToSend.size() > 0){
-                        String s = commandsToSend.remove(0);
-                        System.out.println("SENDING: " + s);
-                        this.out.write(s.getBytes("ASCII"));
-                    }
-                    Thread.sleep(1);
-                }
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-    }
-
-    //process commands receives from the arduino
-    public static class ProcessCommands implements Runnable
-    {
-        public void run ()
-        {
-            try{
-                while(true){
-                    /////sends commandsToSend
-                    while(commandsReceived.size() > 0){
-                        String s = commandsReceived.remove(0);
-                        System.out.println("RECEIVED A COMMAND: " + s);
-                    }
-                    Thread.sleep(1);
-                }
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-    }
-
-    //this processes data so it can be used
+    //this turns strings into commands, e.g. "hi,hihi,fadsl;fa" becomes {"hi," "hihi,"}
     public static class InputsToCommands implements Runnable
     {
         public void run ()
@@ -161,6 +99,108 @@ public class Main {
         }
     }
 
+    ////////////////executing commands
+
+    //executes commands received from the arduino
+    public static class ProcessCommands implements Runnable
+    {
+        public void run ()
+        {
+            try{
+                while(true){
+                    /////sends commandsToSend
+                    while(commandsReceived.size() > 0){
+                        String s = commandsReceived.remove(0);
+                        System.out.println("RECEIVED A COMMAND: " + s);
+                        int i = s.indexOf(":");
+                        if(i!=-1){
+                            String commandString = s.substring(0, i);//the type of command
+                            String arguments = s.substring(i + 1);//the data of the command
+                            /////////////////GPS update command received//////
+                            if(commandString.equals("gps")){
+                                boolean succeeded = true;
+                                double tempx, tempy, tempz;
+                                try{
+
+                                    i = arguments.indexOf(":");
+                                    if(i!=-1){
+                                        x = Double.parseDouble(arguments.substring(0, i));
+                                        arguments = arguments.substring(i + 1);
+                                    }else{
+                                        succeeded = false;
+                                    }
+                                    i = arguments.indexOf(":");
+                                    if(i!=-1){
+                                        y = Double.parseDouble(arguments.substring(0, i));
+                                        arguments = arguments.substring(i + 1);
+                                    }else{
+                                        succeeded = false;
+                                    }
+                                    i = arguments.indexOf(":");
+                                    if(i!=-1){
+                                        z = Double.parseDouble(arguments.substring(0, i));
+//                                        arguments = arguments.substring(i + 1);
+                                    }else{
+                                        succeeded = false;
+                                    }
+                                }catch (Exception e){
+                                    e.printStackTrace();
+                                    succeeded = false;
+                                }
+                                if(succeeded){
+                                    x = tempx;
+                                    y = tempy;
+                                    z = tempz;
+                                    System.out.println("UPDATED GPS: " + tempx + ", " + tempy + ", " + tempz);
+                                }
+                            }else{
+                                System.out.println("The received command did nothing: " + commandString);
+                            }
+                            ////////////////////////////////////////
+                        }
+                    }
+                    Thread.sleep(1);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //this writes out the commandsToSend to the arduino and says "SENDING: "
+    public static class SerialWriter implements Runnable
+    {
+        OutputStream out;
+
+        public SerialWriter ( OutputStream out )
+        {
+            this.out = out;
+
+        }
+
+
+        public void run ()
+        {
+            try{
+
+                while(true){
+                    /////sends commandsToSend
+                    while(commandsToSend.size() > 0){
+                        String s = commandsToSend.remove(0);
+                        System.out.println("SENDING COMMAND: " + s);
+                        this.out.write(s.getBytes("ASCII"));
+                    }
+                    Thread.sleep(1);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    ///////////////////reading data
+
     //this reads console input
     public static class ConsoleCommands implements Runnable
     {
@@ -170,11 +210,41 @@ public class Main {
             try{
 
                 while(true){
-                    consoleInputUnprocessed.add(scanner.next());
+                    String s = scanner.next();
+                    System.out.println("INPUTTED: " + s);
+                    consoleInputUnprocessed.add(s);
                 }
             }catch(Exception e){
                 e.printStackTrace();
             }
         }
     }
+
+    //this reads data
+    private static final class DataListener implements SerialPortDataListener
+    {
+        @Override
+        public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
+
+        @Override
+        public void serialEvent(SerialPortEvent event)
+        {
+            try {
+                //System.out.println("In event listener.");
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+                    return;
+                //System.out.println("Past event type check.");
+                byte[] newData = new byte[comPort.bytesAvailable()];
+                int numRead = comPort.readBytes(newData, newData.length);
+                stringBuffer = new String(newData, "ASCII");//new String(newData,0,numRead);
+                //System.out.println("Read " + numRead + " bytes.");
+                System.out.println("RECIEVED: " + stringBuffer);
+                serialInputUnprocessed.add(stringBuffer);
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
